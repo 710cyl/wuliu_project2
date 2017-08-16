7d32dc36bc9ba4943eb46f729ad5768e48b5a6d0
zaczaczac123 <wcxlikef@163.com>
2017-08-16 15:26:36 +0800

    增加点击窗口写入TextBox界面和指定唯一主键功能

---
+++ domain/Demo1.1.3/Demo1.1.3/1_NewViews/TabbedSections.cs
@@ -0,0 +1,99 @@
+﻿using Demo1._1._3.Panel2_MyWorkBench;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo1._1._3.MyWorkBench_SkipForm
+{
+    public partial class TabbedSections : Form
+    {
+        FunctionClass fc = new FunctionClass();
+        public long total_Page = 0; //页码总条目
+        public long now_Page = 1; //当前页码
+        static public string motorcade_string;//车队
+        static public string carid_string;//车号
+        static public string driver_string;//司机
+        bool IsInternal = true;
+        domain.External_Vehicle ev = new domain.External_Vehicle();
+        domain.Internal_Vehicle iv = new domain.Internal_Vehicle();
+        //定义委托处理
+        public delegate void ClickCar(string a,string b,string c);
+        //定义返回委托类型的事件  
+        public ClickCar ReturnEvent;
+
+        public TabbedSections()
+        {
+            InitializeComponent();
+            domain.Internal_Vehicle iv = new domain.Internal_Vehicle();
+            //gridControl1.DataSource = fc.showData<domain.Internal_Vehicle>(iv, now_Page.ToString());
+            domain.External_Vehicle ev = new domain.External_Vehicle();
+            gridControl1.DataSource = fc.showData<domain.External_Vehicle>(ev, now_Page.ToString());
+        }
+
+        //取消
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        //查询事件
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            String input_val = textBox1.Text;
+            if (IsInternal == false)
+            {
+                if (input_val.Equals(""))
+                {
+                    MessageBox.Show("请输入查询关键字！");
+                }
+                else
+                {
+                    gridControl1.DataSource = fc.showDataLike < domain.External_Vehicle> (ev, now_Page.ToString(), input_val);
+                }
+            }
+            else 
+            {
+                if (input_val.Equals(""))
+                {
+                    MessageBox.Show("请输入查询关键字！");
+                }
+                else
+                {
+                    gridControl1.DataSource = fc.showDataLike<domain.Internal_Vehicle>(iv, now_Page.ToString(), input_val);
+                }
+            }
+        }
+        //确定事件
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            motorcade_string = gridView1.GetFocusedRowCellDisplayText(gridView1.Columns[1]);
+            carid_string = gridView1.GetFocusedRowCellDisplayText(gridView1.Columns[2]);
+            driver_string = gridView1.GetFocusedRowCellDisplayText(gridView1.Columns[3]);
+            Close();
+            ReturnEvent?.Invoke(motorcade_string, carid_string, driver_string);
+            DriverCheck dc = new DriverCheck();
+
+
+        }
+        //内部车队事件
+        private void accordionControlElement2_Click_1(object sender, EventArgs e)
+        {
+            IsInternal = true;
+            domain.Internal_Vehicle iv = new domain.Internal_Vehicle();
+            gridControl1.DataSource = fc.showData<domain.Internal_Vehicle>(iv, now_Page.ToString());
+        }
+        //外部车队事件
+        private void accordionControlElement4_Click(object sender, EventArgs e)
+        {
+            IsInternal = false;
+            domain.External_Vehicle ev = new domain.External_Vehicle();
+            gridControl1.DataSource = fc.showData<domain.External_Vehicle>(ev, now_Page.ToString());
+        }
+
+
+    }
+}
---
+++ domain/Demo1.1.3/Demo1.1.3/1_NewViews/TabbedSections.designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Demo1._1._3.MyWorkBench_SkipForm
+{
+    partial class TabbedSections
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.accordionControl1 = new DevExpress.XtraBars.Navigation.AccordionControl();
+            this.accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.accordionControlElement4 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.accordionControlElement2 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.accordionControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Image = global::Demo1._1._3.Properties.Resources.add_16x16;
+            this.simpleButton1.Location = new System.Drawing.Point(809, 12);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "确定";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Image = global::Demo1._1._3.Properties.Resources.cancel_16x16;
+            this.simpleButton2.Location = new System.Drawing.Point(890, 12);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "取消";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(260, 47);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(720, 433);
+            this.gridControl1.TabIndex = 2;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Image = global::Demo1._1._3.Properties.Resources.lookup_reference_16x16;
+            this.simpleButton3.Location = new System.Drawing.Point(196, 12);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton3.TabIndex = 3;
+            this.simpleButton3.Text = "查询";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.simpleButton3);
+            this.panel1.Controls.Add(this.simpleButton1);
+            this.panel1.Controls.Add(this.simpleButton2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(980, 47);
+            this.panel1.TabIndex = 5;
+            // 
+            // accordionControl1
+            // 
+            this.accordionControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.accordionControlElement1});
+            this.accordionControl1.Location = new System.Drawing.Point(0, 47);
+            this.accordionControl1.Name = "accordionControl1";
+            this.accordionControl1.Size = new System.Drawing.Size(260, 433);
+            this.accordionControl1.TabIndex = 6;
+            this.accordionControl1.Text = "accordionControl1";
+            // 
+            // accordionControlElement1
+            // 
+            this.accordionControlElement1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.accordionControlElement4,
+            this.accordionControlElement2});
+            this.accordionControlElement1.Expanded = true;
+            this.accordionControlElement1.Name = "accordionControlElement1";
+            this.accordionControlElement1.Text = "车队";
+            // 
+            // accordionControlElement4
+            // 
+            this.accordionControlElement4.Name = "accordionControlElement4";
+            this.accordionControlElement4.Text = "外部车队";
+            this.accordionControlElement4.Click += new System.EventHandler(this.accordionControlElement4_Click);
+            // 
+            // accordionControlElement2
+            // 
+            this.accordionControlElement2.Name = "accordionControlElement2";
+            this.accordionControlElement2.Text = "内部车队";
+            this.accordionControlElement2.Click += new System.EventHandler(this.accordionControlElement2_Click_1);
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2010 Silver";
+            // 
+            // TabbedSections
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 480);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.accordionControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "TabbedSections";
+            this.Text = "列表选择";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.accordionControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraBars.Navigation.AccordionControl accordionControl1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement4;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement2;
+    }
+}
